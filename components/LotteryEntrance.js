750d8935426b9faa78af19c8b3c2f919af4eca19
Yves Tscherry <info@feerglas.com>
2022-06-03 10:50:23 +0200

    add basic styling with tailwind

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -10,10 +10,16 @@ export default function LotteryEntrance() {
   const raffleAddress =
     chainId in contractAddresses ? contractAddresses[chainId][0] : null;
   const [entranceFee, setEntranceFee] = useState('0');
+  const [numPlayers, setNumPlayers] = useState('0');
+  const [recentWinner, setRecentWinner] = useState('0');
 
   const dispatch = useNotification();
 
-  const { runContractFunction: enterRaffle } = useWeb3Contract({
+  const {
+    runContractFunction: enterRaffle,
+    isLoading,
+    isFetching,
+  } = useWeb3Contract({
     abi: abi,
     contractAddress: raffleAddress,
     functionName: 'enterRaffle',
@@ -28,13 +34,31 @@ export default function LotteryEntrance() {
     params: {},
   });
 
+  const { runContractFunction: getNumberOfPlayers } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: 'getNumberOfPlayers',
+    params: {},
+  });
+
+  const { runContractFunction: getRecentWinner } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: 'getRecentWinner',
+    params: {},
+  });
+
+  const updateUi = async () => {
+    const entranceFeeFromCall = (await getEntranceFee()).toString();
+    const numPlayersFromCall = (await getNumberOfPlayers()).toString();
+    const recentWinnerFromCall = await getRecentWinner();
+    setEntranceFee(entranceFeeFromCall);
+    setNumPlayers(numPlayersFromCall);
+    setRecentWinner(recentWinnerFromCall);
+  };
+
   useEffect(() => {
     if (isWeb3Enabled) {
-      const updateUi = async () => {
-        const entranceFeeFromCall = (await getEntranceFee()).toString();
-        setEntranceFee(entranceFeeFromCall);
-      };
-
       updateUi();
     }
   }, [isWeb3Enabled]);
@@ -52,16 +76,24 @@ export default function LotteryEntrance() {
   const handleSuccess = async (tx) => {
     await tx.wait(1);
     handleNewNotification(tx);
+    updateUi();
   };
 
   return (
-    <div>
+    <div className="p-5">
       Lottery entrance
+      <br />
       {raffleAddress ? (
         <div>
           Entrance Fee: {ethers.utils.formatUnits(entranceFee, 'ether')} ETH
           <br />
+          Number of players: {numPlayers}
+          <br />
+          Recent winner: {recentWinner}
+          <br />
           <button
+            disabled={isLoading || isFetching}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
             onClick={async () => {
               await enterRaffle({
                 onSuccess: handleSuccess,
@@ -69,7 +101,11 @@ export default function LotteryEntrance() {
               });
             }}
           >
-            Enter Raffle
+            {isLoading || isFetching ? (
+              <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+            ) : (
+              <div>Enter Raffle</div>
+            )}
           </button>
         </div>
       ) : (
